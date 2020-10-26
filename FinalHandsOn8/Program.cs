fc1cddfd05be9df4322b9b8bc3368cb0f093c6ab
jhilton642 <joshua.hilton@att.net>
2020-10-26 14:27:18 -0500

    Add project files.

---
+++ FinalHandsOn8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalHandsOn8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userStrings = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Enter a string. Enter an empty string to exit");
+                string userInput = Console.ReadLine();
+                if (userInput.Length == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    userStrings.Add(userInput);
+                }
+            }
+            string longestString = userStrings[0];
+            string shorteststring = userStrings[0];
+            userStrings.Sort();
+            string earliestString = userStrings[0];
+            string latestString = userStrings[userStrings.Count-1];
+            foreach (var item in userStrings)
+            {
+                if (item.Length > longestString.Length)
+                {
+                    longestString = item;
+                }
+                if (item.Length < shorteststring.Length)
+                {
+                    shorteststring = item;
+                }
+            }
+            Console.WriteLine("The longest string is: " + longestString);
+            Console.WriteLine("The shortest string is: " + shorteststring);
+            Console.WriteLine("The earliest string is: " + earliestString);
+            Console.WriteLine("The latest string is: " + latestString);
+        }
+    }
+}
